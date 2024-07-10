c9d63427acda4991b19ea88c2d62e7d4034cd5a5
Nagashree3007 <crnagashree8@gmail.com>
2024-07-10 13:25:51 +0530

    [Nagashree]:the number is equal to the sum of its positive divisors excluding the number itself.

---
+++ logical_programs/p2_perfect numbers.py
@@ -0,0 +1,20 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-11 
+@Last Modified by: Author Name
+@Last Modified: 2024-07-11 
+@Title : Program Aim
+'''
+
+def perfect(n):
+  sum_n=0
+  for i in range(1,n+1):
+    if i % n==0:
+      sum_n +=i
+    if sum_n==n:
+      print("perfect number")
+    elif sum_n>n:
+      print("not perfect number")
+n=int(input())
+perfect(n)
+
