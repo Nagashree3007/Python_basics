c21cffc80b44c7381119a675f3bcb0ea99bf497f
Nagashree3007 <145009353+Nagashree3007@users.noreply.github.com>
2024-07-11 11:36:57 +0530

    p10_monthlypayment.py

--- logical_programs/p10_monthlypayment.py
+++ logical_programs/p10_monthlypayment.py
@@ -1,9 +1,9 @@
 '''
 @Author: Nagashree C R
-@Date: 2024-07-11 
-@Last Modified by: Author Name
-@Last Modified: 2024-07-11 
-@Title : Program Aim
+@Date: 2024-07-10 
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-10 
+@Title : Find the monthly payment should be done based on pI,year,Rate of intrest.
 '''
 
 class util:
