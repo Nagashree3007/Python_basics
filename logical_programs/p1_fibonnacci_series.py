6c8b207f1a2ec1a7c7b5783b961ae2b4726f814f
Nagashree3007 <145009353+Nagashree3007@users.noreply.github.com>
2024-07-11 11:20:44 +0530

    p1_fibonnacci_series.py

--- logical_programs/p1_fibonnacci_series.py
+++ logical_programs/p1_fibonnacci_series.py
@@ -1,3 +1,11 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-10 
+@Last Modified by: Author Name
+@Last Modified: 2024-07-10 
+@Title : Find the fibonnaci of a number
+'''
+
 def fibonnacci(n):
   a=0
   b=1
