bb9deebcb7bf8b076daa0121a160c8b26fc9a905
Nagashree3007 <crnagashree8@gmail.com>
2024-07-11 12:34:32 +0530

    [Nagashree] Add:the prime factorization of N using brute force.

--- Basic_core_programs/p5_factors.py
+++ Basic_core_programs/p5_factors.py
@@ -1,3 +1,11 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-11
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-11
+@Title : the prime factorization of N using brute force.
+'''
+
 def prime_fact(n):
   fact=[]
   divisor=2
