ea18188c67013713a2bae207610285c93ea0e9cb
Nagashree3007 <145009353+Nagashree3007@users.noreply.github.com>
2024-07-11 21:05:09 +0530

    [Nagashree] Add: Read an integer as an Input, convert to Binary using  toBinary  function and perform the following functions. i. Swap nibbles and find the new number. ii. Find the resultant number is the number is a power of 2.

--- logical_programs/p13_swapnibble.py
+++ logical_programs/p13_swapnibble.py
@@ -1,7 +1,7 @@
 '''
 @Author: Nagashree C R
 @Date: 2024-07-10 
-@Last Modified by: Nagashree C R
+@Last Modified by: Nagashree C R 
 @Last Modified: 2024-07-10 
 @Title :read an integer as an Input, convert to Binary using toBinary function and perform the following functions.
 i. Swap nibbles and find the new number.
