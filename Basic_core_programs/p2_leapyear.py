5c0571ea680b051cfb2251e21744167a447ba24f
Nagashree3007 <145009353+Nagashree3007@users.noreply.github.com>
2024-07-11 12:29:16 +0530

    p2_leapyear.py

--- Basic_core_programs/p2_leapyear.py
+++ Basic_core_programs/p2_leapyear.py
@@ -1,3 +1,11 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-11
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-11
+@Title : find the given year is leap year or not.
+'''
+
 def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
