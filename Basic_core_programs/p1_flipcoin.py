d575d3add88a48a3c7d47b3509cd86c96c4ed266
Nagashree3007 <crnagashree8@gmail.com>
2024-07-11 12:06:08 +0530

    [Nagashree] Add:find the percentage of head and tail for n number of flips of coin.

--- Basic_core_programs/p1_flipcoin.py
+++ Basic_core_programs/p1_flipcoin.py
@@ -1,13 +1,34 @@
-def is_leap_year(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+'''
+@Author: Nagashree C R
+@Date: 2024-07-11
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-11
+@Title : find the peercentage of head and tail after fliping a coin n number of time.
+'''
 
-year =input()
-if len(year)==4:
-  year=int(year)
-  is_leap_year(year)
-  if is_leap_year(year):
-    print(f"{year} is a leap year.")
-  else:
-    print(f"{year} is not a leap year.")
-else:
-  print("invalid input....enter only four digit number")
+import random
+import math
+n=int(input())
+head=0
+tail=0
+def count_val(n):
+  head=0
+  tail=0
+  for val in range(n):
+    random_number = random.random()
+    print(f'random number between 1 and 0 is {random_number}')
+    if(random_number<0.5):
+      tail+=1
+      print(f" no of tail {tail}")
+    else:
+      head+=1
+      print(f"no of head {head}")
+  print(f'tatal number of heads abd tails {head},{tail}')
+  sum=head+tail
+  #sum
+  print(sum)
+  #calculating percentage
+  tail_percentage=(tail/sum)*100
+  head_percentage=(head/sum)*100
+  print(f"percentage of Head is{head_percentage} and Tail is {tail_percentage}")
+count_val(n)
