76e97cb5ed5908804f6cfc60de80af16c92a35d1
Nagashree3007 <crnagashree8@gmail.com>
2024-07-11 12:20:53 +0530

    [Nagashree] Add:swaping two numbers using third variable.

---
+++ Basic_core_programs/p7_swaping.py
@@ -0,0 +1,14 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-11
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-11
+@Title : swaping two numbers using third variable.
+'''
+
+def swap(a,b):
+  temp=a
+  a=b
+  b=temp
+  print(f'a={a},b={b}')
+swap(30,50)
