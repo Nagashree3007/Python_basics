bbaafed851ec99d7cb5231a339a38bebe47155f1
Nagashree3007 <crnagashree8@gmail.com>
2024-07-11 13:33:07 +0530

    [Nagashree] find given year is prime or not

--- Basic_core_programs/p2_leapyear.py
+++ Basic_core_programs/p2_leapyear.py
@@ -8,12 +8,12 @@
 
 def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
+  
 year =input()
-if len(year)==4:
+if len(year)==4:   
   year=int(year)
   is_leap_year(year)  
-  if is_leap_year(year):
+  if is_leap_year(year):  
     print(f"{year} is a leap year.")
   else:
     print(f"{year} is not a leap year.")
