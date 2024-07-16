e42a15aefc7a8d986b00395fc73e6ddd9080be7a
Nagashree3007 <145009353+Nagashree3007@users.noreply.github.com>
2024-07-16 21:25:36 +0530

    [Nagashree] Add :Find distinct coupon numbers using random function

--- logical_programs/p5_Coupon numbers.py
+++ logical_programs/p5_Coupon numbers.py
@@ -4,17 +4,24 @@
 @Last Modified by: Nagashree C R
 @Last Modified: 2024-07-10 
 @Title : Find distinct coupon numbers using random function
-'''
-import random 
-def coupon(n):
-  count=0
-  existing=[]
-  for i in range(1,100):
-    coupon=random.randint(1,n)
-    count+=1
-    existing.append(coupon)
-    set(existing)
-    if len(existing)==n:
-      print(f'no of cupon count{count},distinct coupon are {existing}')
 
-coupon(10)
+'''
+import random
+
+def coupon(n):
+  lucky_number=[]
+  for i in range(0,n):
+    number=random.randint(1111,5555)
+    lucky_number.append(number)
+  check=int(input("Enter your coupon number"))
+  x=','.join(str(i) for i in lucky_number)
+  print(f'the lucky coupon numbers are  {x}')
+  print(f'your coupon number {check}')
+  if check in lucky_number:
+    print("congratulations you won the lucky coupon")
+  else:
+    print("opps!!!..you lost,better luck next time")
+
+numberofcoupons=int(input("enter the number of coupons to be generated"))
+
+coupon(numberofcoupons)
